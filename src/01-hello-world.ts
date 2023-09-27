618d2c2de17ae41d9d6528d5f21a65ffa0f0bed9
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-26 20:04:35 -0600

    First TypeScript files with hello world and demo code with async/await instruction, made first transpilations form .ts files to .js files

---
+++ src/01-hello-world.ts
@@ -0,0 +1,3 @@
+// Este código de TypeScript no se ejecuta directamente en el navegador, sino que se compila a JavaScript para que el navegador lo entienda.
+const myName = "Fernando";
+console.log(myName);
