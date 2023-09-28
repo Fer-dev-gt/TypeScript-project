2cfb5205e8787f6218c2f640f31407bb6075e778
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-27 21:10:09 -0600

    Made multiple number declarations examples and number-like values, alse simulate cases where an error might happen

--- src/04-numbers.ts
+++ src/04-numbers.ts
@@ -6,19 +6,19 @@
 
 
   let customerAge: number = 27;
-  customerAge = customerAge + 1;                                // Esto si lo permite TypeScript
-  // customerAge = customerAge + '1';                           // Esto no lo permite TypeScript, en JavaScript el output seria '281' pero en TypeScript no
+  customerAge = customerAge + 1;                                  // Esto si lo permite TypeScript
+  // customerAge = customerAge + '1';                             // Esto no lo permite TypeScript, en JavaScript el output seria '281' pero en TypeScript no
   console.log('customerAge: ', customerAge);
 
 
   // Si solo queremos declarar una variable y no asignarle un valor, obligatoriamente debemos especificar el tipo de dato
   let productsInStock: number;
-  console.log('productsInStock: ', productsInStock);            // Esto nos dara 'undefined' en JavaScript, pero en TypeScript nos dara un error ya que no se le ha asignado un valor, este erro no impedira que se ejecute el codigo javascript pero si nos dara un error en la consola de TypeScript
-  if(productsInStock > 10) {                                    // Aca tambien nos dara un error ya que no se le ha asignado un valor a la variable 'productsInStock'
+  console.log('productsInStock: ', productsInStock);              // Esto nos dara 'undefined' en JavaScript, pero en TypeScript nos dara un error ya que no se le ha asignado un valor, este erro no impedira que se ejecute el codigo javascript pero si nos dara un error en la consola de TypeScript
+  if(productsInStock > 10) {                                      // Aca tambien nos dara un error ya que no se le ha asignado un valor a la variable 'productsInStock'
     console.log('is available');
   }
 
-  let discount = parseInt('100');                               // Al usar 'parseInt' tomamos un valor de tipo 'string' y lo convertimos a 'number' esto es algo que no se puede hacer en JavaScript
+  let discount = parseInt('100');                                 // Al usar 'parseInt' tomamos un valor de tipo 'string' y lo convertimos a 'number' esto es algo que no se puede hacer en JavaScript
   console.log('discount: ', discount);
   if(discount <= 200) {
     console.log('discount is a not NaN');
